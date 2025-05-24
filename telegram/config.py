d7d3e537caaec12c5249f69cee5e65b6c8946ec6
Neverland <snosebala@vk.com>
2025-05-24 23:38:15 +0300

    add on_time_link

--- telegram/config.py
+++ telegram/config.py
@@ -1,5 +1,6 @@
 class Config:
     api_key: str = '8055071840:AAGsPS969Hs24mQIIe7K7c1DYcdd6CRzto0'
     chat_id: int = -1002689647822
-
+    on_time_parser_link: str = 'https://www.avito.ru/moskva/telefony/mobilnye_telefony/apple/iphone_11-ASgBAgICA0SywA2kmzm0wA3OqzmwwQ2I_Dc?cd=1&f=ASgBAQECBESuwA36gjiywA2kmzm0wA3OqzmwwQ2I_DcBQOjrDiT8_dsC~v3bAgFFxpoMFXsiZnJvbSI6MCwidG8iOjEyNTAwfQ&s=104'
+    on_cost_parser_link: str
 config = Config()
